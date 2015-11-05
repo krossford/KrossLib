56af52cdaca951ac0109a602f995803b9e6e0e80
krosshuang <krossford@foxmail.com>
2015-11-05 20:19:59 +0800

    add drawerlayout and add some common color, dimen xml

--- myphotoshop/src/main/java/com/krosshuang/myphotoshop/controller/MainActivity.java
+++ myphotoshop/src/main/java/com/krosshuang/myphotoshop/controller/MainActivity.java
@@ -1,16 +1,40 @@
-package com.krosshuang.myphotoshop;
+package com.krosshuang.myphotoshop.controller;
 
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.krosshuang.myphotoshop.R;
 
 public class MainActivity extends AppCompatActivity {
+
+    private String[] mData = new String[]{
+            "新建",
+    };
+
+    private DrawerLayout mDrawerLayout = null;
+    private ListView mMenuListView = null;
+
+    private MenuDrawerListAdapter mAdapter = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initView();
+    }
+
+    private void initView() {
+        mDrawerLayout = (DrawerLayout)findViewById(R.id.drawer_layout);
+        mMenuListView = (ListView)findViewById(R.id.left_drawer);
+
+        mAdapter = new MenuDrawerListAdapter(this, mData);
+        mMenuListView.setAdapter(mAdapter);
 
     }
 
