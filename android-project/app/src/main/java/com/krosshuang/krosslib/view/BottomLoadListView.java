a0133ad30aebe2011d9ccf3fbb5d375eb56455b0
krosshuang_guangzhou_b5 <670283005@qq.com>
2015-12-15 22:04:01 +0800

    删除plugin项目

---
+++ android-project/app/src/main/java/com/krosshuang/krosslib/view/BottomLoadListView.java
@@ -0,0 +1,16 @@
+package com.krosshuang.krosslib.view;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.ListView;
+
+/**
+ * 底部加载ListView
+ * Created by krosshuang on 2015/12/15.
+ */
+public class BottomLoadListView extends ListView {
+
+    public BottomLoadListView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+}
